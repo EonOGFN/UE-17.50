607e7e358a51a60c7705b34801880af246d97743
Admin <EonOGFN@users.noreply.github.com>
2026-08-13 21:56:14 +0300

    Initial Commit

---
+++ Engine/Plugins/FX/Niagara/Source/NiagaraShader/Private/NiagaraShaderDerivedDataVersion.h
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+/*=============================================================================
+NiagaraShaderDerivedDataVersion.h: Shader derived data version for Niagara.
+=============================================================================*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+// In case of merge conflicts with DDC versions, you MUST generate a new GUID and set this new
+// guid as version
+
+#define NIAGARASHADERMAP_DERIVEDDATA_VER		TEXT("6584DF60-2F6E-4248-997B-CF94CEC61E13")
